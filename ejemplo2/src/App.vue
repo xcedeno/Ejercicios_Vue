aa728592f363d83e2a1b113133aa913df07c1548
JuanDuran85 <duran85juan@gmail.com>
2020-04-05 18:27:10 -0400

    modificando ejemplo2 eventos

--- ejemplo2/src/App.vue
+++ ejemplo2/src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" v-model="inicio" @keyup="cambioContador">
+    <button @click="sumar">Sumar 1</button>
+    <p v-text="contador"></p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      contador: 0,
+      inicio: 0
+    }
+  },
+  methods: {
+    sumar(){
+      this.contador++;
+    },
+    cambioContador(){
+      this.contador = this.inicio;
+    }
+  },
 }
 </script>
 
@@ -22,7 +32,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 2em;
 }
 </style>
