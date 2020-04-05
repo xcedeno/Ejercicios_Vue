8ea5a7b9c908eb2257fe39a61c194860cadc4fd1
JuanDuran85 <duran85juan@gmail.com>
2020-04-05 18:48:14 -0400

    modificando ejemplo2 eventos

--- ejemplo2/src/App.vue
+++ ejemplo2/src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
     <input type="text" v-model="inicio" @keyup="cambioContador">
-    <button @click="sumar">Sumar 1</button>
+    <button @click="sumar(2)">Sumar + 2</button>
     <p v-text="contador"></p>
+    <p @mousemove="valoresXY">
+      valor de x: {{x}} <br>
+      valor de y: {{y}}
+    </p>
   </div>
 </template>
 
@@ -12,15 +16,21 @@ export default {
   data() {
     return {
       contador: 0,
-      inicio: 0
+      inicio: 0,
+      x: 0,
+      y: 0
     }
   },
   methods: {
-    sumar(){
-      this.contador++;
+    sumar(dos){
+      this.contador += dos;
     },
     cambioContador(){
       this.contador = this.inicio;
+    },
+    valoresXY(evento){
+      this.x = evento.clientX;
+      this.y = evento.clientY;
     }
   },
 }
