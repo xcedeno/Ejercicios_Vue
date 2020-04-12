5a6c81d2857f889c0cc977200ecc13d8fc33b5e9
JuanDuran85 <duran85juan@gmail.com>
2020-04-11 20:06:31 -0400

    agregando ejemplo20 aplicacion basica de nota

---
+++ ejemplo20/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+     <p>Mostrando</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
