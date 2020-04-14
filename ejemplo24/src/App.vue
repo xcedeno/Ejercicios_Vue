ba7b17b07dfff6b79f284bc14bd39c92145f34a1
JuanDuran85 <duran85juan@gmail.com>
2020-04-14 16:13:54 -0400

    agregando ejemplo24 - gestionde formularios

---
+++ ejemplo24/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>App VueJS</h1>
+    <Formulario/>
+  </div>
+</template>
+
+<script>
+import Formulario from './components/Formulario.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Formulario
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ ejemplo24/src/components/Formulario.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <h1>Formulario</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Formulario'       
+}
+</script>
+
+<style>
+</style>
