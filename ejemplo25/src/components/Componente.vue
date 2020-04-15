7c6c530c87c5a0a561a5fcc0204d783cff5189bd
JuanDuran85 <duran85juan@gmail.com>
2020-04-15 11:02:16 -0400

    modificando ejemplo25 - uso de vuex

---
+++ ejemplo25/src/components/Componente.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>Componente</h1>
+    <p>{{$store.state.mensaje}}</p>
+    <p>Llamando desde el getters</p>
+    <p>{{$store.getters.llamadoVariables}}</p>
+    <hr>
+    <h3>{{$store.getters.nombreCompleto}}</h3>
+    <h2>Objetos:</h2>
+    <input type="text" v-model="objeto">
+    <button @click="agregandoObjeto">Agregar</button>
+    <ul>
+      <li v-for="(item, index) in $store.state.objetos" :key="index">
+        {{item}} <button @click="borrar(index)">Eliminar</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Componente',
+  data() {
+    return {
+      objeto: ''
+    }
+  },
+  methods: {
+    agregandoObjeto(){
+      this.$store.dispatch('agregandoObjeto',this.objeto);
+      this.objeto = '';
+    },
+    borrar(index){
+      this.$store.commit('borrarObjeto',index);
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- ejemplo25/src/store/index.js
+++ ejemplo25/src/store/index.js
@@ -1,15 +1,36 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    mensaje: 'Envio de información desde el estado de Vuex',
+    nombre: 'Vue',
+    apellido: 'JS',
+    objetos: [],
+    objeto: null
+  },
+  getters: {
+    llamadoVariables(state){
+      console.log(state);
+      return state.mensaje;
+    },
+    nombreCompleto(state){
+      return `${state.nombre} ${state.apellido}`;
+    }
   },
   mutations: {
+    agregarObjetos(state,objeto2){
+      state.objetos = [objeto2, ...state.objetos];
+    },
+    borrarObjeto(state,index){
+      state.objetos.splice(index,1);
+    }
   },
   actions: {
-  },
-  modules: {
+    agregandoObjeto(context,objeto2) {
+      context.commit('agregarObjetos',objeto2);
+    }
   }
-})
+});
