41271de2bb65905ebdc252afeba8b1f44270bbe6
JuanDuran85 <duran85juan@gmail.com>
2020-04-19 00:29:14 -0400

    modifiando el ejemplo27

--- ejemplo27/assets/js/script.js
+++ ejemplo27/assets/js/script.js
@@ -8,9 +8,9 @@ Vue.component("componente1", {
                 <div class="product-info">
                     <h1 v-text="producto"></h1>
                     <a :href="mas" target="_blank">Mas información aquí</a>
-                    <p v-if="stock > 10">En Inventario</p>
-                    <p v-else-if="stock <= 10 && stock > 0">Últimas unidades</p>
-                    <p v-else>Sin inventario</p>
+                    <p v-if="variantes[seleccion].stock > 10">En Inventario</p>
+                    <p v-else-if="variantes[seleccion].stock <= 10 && variantes[seleccion].stock > 0">Últimas unidades</p>
+                    <p v-else :class="{noInven: stockZero}">Sin inventario</p>
                     <ul>
                         <li v-for="(datalle,index) in detalles" :key="index">
                             {{datalle}}
@@ -18,8 +18,7 @@ Vue.component("componente1", {
                     </ul>
                     <p>Variantes</p>
                     <ul>
-                        <li v-for="(variante,index) in variantes" :key="index" @mouseover="cambioColor(variante.variantId)">
-                            {{variante.variantColor}}
+                        <li v-for="(variante,index) in variantes" :key="index" @mouseover="cambioColor(index)" class="color-box" :style="{backgroundColor: variante.variantColor }">
                         </li>
                     </ul>
                     <p>Tallas</p>
@@ -28,7 +27,7 @@ Vue.component("componente1", {
                             {{talla}}
                         </li>
                     </ul>
-                    <button @click="comprar" type="button">Agregar al Carro</button>
+                    <button @click="comprar" type="button" :disabled="stockZero" :class="{disabledButton: stockZero}">Agregar al Carro</button>
                     <button @click="quitar" type="button">Quitar del Carro</button>
                     <div class="cart">
                         Cart:({{carro}})
@@ -40,21 +39,26 @@ Vue.component("componente1", {
     data() {
         return {
             producto: "Medias",
-            image: 'assets/img/medias.jpg',
+            seleccion: 0,
+            image: 'assets/img/mediasNegras.jpg',
             alter: 'par de medias culquiera',
             mas: 'https://www.google.com/',
             stock: 2,
             detalles: ["80% algodon", "20% poliester", "Para cualquier temporada"],
             variantes: [
                 {
-                  variantId: 2234,
-                  variantColor: 'Negro',
-                  variantImage: 'assets/img/mediasNegras.jpg'    
+                    id: 0,
+                    variantId: 2234,
+                    variantColor: 'black',
+                    variantImage: 'assets/img/mediasNegras.jpg',
+                    stock: 20  
                 },
                 {
-                  variantId: 2235,
-                  variantColor: 'Gris',
-                  variantImage: 'assets/img/mediasGris.jpg'
+                    id:1,
+                    variantId: 2235,
+                    variantColor: 'gray',
+                    variantImage: 'assets/img/mediasGris.jpg',
+                    stock: 2
                 }
             ],
             medidas: ["M","S","L"],
@@ -63,20 +67,35 @@ Vue.component("componente1", {
     },
     methods: {
         comprar(){
-            this.carro++;
-        },
-        cambioColor(dato){
-            let auxiliar = this.variantes.filter((valor)=>valor.variantId == dato);
-            auxiliar.forEach(element => {
-                this.image = element.variantImage;
+            let auxiliar2 = this.variantes.filter((valor)=>valor.variantImage == this.image);
+            auxiliar2.forEach(element => {
+                if (element.stock > 0) {
+                    element.stock--;
+                    this.carro++;
+                }else{
+                    alert("No existe stock del producto");
+                }
             });
         },
+        cambioColor(dato){
+            this.image = this.variantes[dato].variantImage;
+            this.seleccion = dato;
+        },
         quitar(){
-            if (this.carro>0) {
-                this.carro--;
-            }else{
-                alert("No se puede descontar");
-            }
+            let auxiliar3 = this.variantes.filter((valor)=>valor.variantImage == this.image);
+            auxiliar3.forEach(element => {
+                if (this.carro > 0 && this.seleccion == element.id) {
+                    element.stock++;
+                    this.carro--;
+                }else{
+                    alert("No existe producto en el carro. Seleccione el color adecuado");
+                }
+            });
+        }
+    },
+    computed: {
+        stockZero(){
+            return this.variantes[this.seleccion].stock == 0;
         }
     },
 });
