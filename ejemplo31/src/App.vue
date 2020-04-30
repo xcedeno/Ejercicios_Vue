91b1f2b4c7dcab2066ec9bf67a9078f90b800689
JuanDuran85 <duran85juan@gmail.com>
2020-04-29 23:02:48 -0400

    agregando ejemplo31 - armando un objeto

---
+++ ejemplo31/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img src="./assets/build-a-bot-logo.png" alt="logo-robot" class="logo">
+            Construye un Robot
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <HomePage/>
+    </main>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+header {
+  background-color: #999;
+  width: 80%;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+main{
+  margin: 0 auto;
+  background-color: white;
+  width: 80%;
+  min-height: 400px;
+}
+</style>
