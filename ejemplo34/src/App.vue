d34117dc0c1d90d9de7b1b84b841a574d7f46baf
JuanDuran85 <duran85juan@gmail.com>
2020-05-10 12:58:58 -0400

    modificando ejemplo34 directivas locales

--- ejemplo34/src/App.vue
+++ ejemplo34/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container">
     <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
-      <p v-colorTexto.cursiva>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt perferendis veniam natus et <span v-tipoTexto.negrita = "'Times New Roman'">explicabo praesentium modi quas</span>, voluptas doloribus, maxime commodi atque debitis dicta <span v-tipoTexto.capitalize = "'helvetica'">voluptates necessitatibus adipisci maiores, distinctio odit?</span></p>
+      <p v-colorTexto.cursiva>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt perferendis veniam natus et <span v-tipoTexto:mayor.negrita = "'Times New Roman'">explicabo praesentium modi quas</span>, <span v-transforTexto="{colores: 'green'}">voluptas doloribus, maxime commodi</span> atque debitis dicta <span v-tipoTexto:menor.capitalize = "'helvetica'">voluptates necessitatibus adipisci maiores, distinctio odit?</span></p>
+      <p><span v-cambioLocal.negrita>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem corrupti laudantium, <span v-cambioLocal.uppercase>non alias nisi error, eos similique</span> officia, enim doloremque quaerat dolorem optio! Tempore assumenda reprehenderit numquam vero vel!</span></p>
       <b-button variant="primary" href="#">Clic</b-button>
     </b-jumbotron>
   </div>
@@ -11,6 +12,17 @@
 
 export default {
   name: 'App',
+  directives: {
+    'cambioLocal' : {
+      bind(el,binding){
+        if (binding.modifiers['negrita']) {
+          el.style.fontWeight = 'bold';
+        } else if (binding.modifiers['uppercase']) {
+          el.style.textTransform = 'uppercase';
+        }
+      }
+    }
+  }
 }
 </script>
 
--- ejemplo34/src/main.js
+++ ejemplo34/src/main.js
@@ -2,20 +2,26 @@ import Vue from 'vue';
 import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
+// Directivas globales
 Vue.directive('colorTexto', {
   bind(el,binding){
     el.style.color = 'blue';
     if (binding.modifiers['cursiva']) {
       el.style.fontStyle = 'italic';
     }
+  }
+});
+
+Vue.directive('transforTexto', {
+  bind(el,binding){
+    el.style.color = binding.value.colores;
   }
 })
 
@@ -29,9 +35,15 @@ Vue.directive('tipoTexto', {
     } else if (binding.modifiers['capitalize']) {
       el.style.textTransform = 'capitalize';
     }
+
+    if (binding.arg == 'menor') {
+      el.style.fontSize = '10px';
+    }
+    if (binding.arg == 'mayor') {
+      el.style.fontSize = '34px';
+    }
   }
 })
-
 
 new Vue({
   render: h => h(App),
