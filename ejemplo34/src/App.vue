74a05e7501bdfdf0c1feae12413bc2851c3888d3
JuanDuran85 <duran85juan@gmail.com>
2020-05-18 17:04:40 -0400

    modificando ejemplo34 directivas personalizadas

--- ejemplo34/src/App.vue
+++ ejemplo34/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
+    <b-jumbotron header="Directivas" lead="Directivas locales y globales personalizadas">
       <p v-colorTexto.cursiva>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt perferendis veniam natus et <span v-tipoTexto:mayor.negrita = "'Times New Roman'">explicabo praesentium modi quas</span>, <span v-transforTexto="{colores: 'green'}">voluptas doloribus, maxime commodi</span> atque debitis dicta <span v-tipoTexto:menor.capitalize = "'helvetica'">voluptates necessitatibus adipisci maiores, distinctio odit?</span></p>
       <p><span v-cambioLocal.negrita>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem corrupti laudantium, <span v-cambioLocal.uppercase>non alias nisi error, eos similique</span> officia, enim doloremque quaerat dolorem optio! Tempore assumenda reprehenderit numquam vero vel!</span></p>
       <b-button variant="primary" href="#">Clic</b-button>
