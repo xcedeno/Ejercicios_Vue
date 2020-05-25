42afb22b3ee072d4b5c7a45e91aa92252aa5ab27
JuanDuran85 <duran85juan@gmail.com>
2020-05-24 23:26:44 -0400

    modificando ejemplo52 - conversor de monedas

--- ejemplo52/src/conexiones/api_fixer.js
+++ ejemplo52/src/conexiones/api_fixer.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import accounting from 'accounting';
-import fx from 'money'
+import fx from 'money';
 
 const fixerURL = 'http://data.fixer.io/api/latest';
 const access_key = '310d239b67a8132ff4e850e1df2f71e7';
@@ -11,14 +11,11 @@ const api_id = "f0ce15a803ac4bb9a8a42b2e09427ece";
 export function monedaFixer() {
     axios.get(`${fixerURL}?access_key=${access_key}`)
     .then(response => {
-        console.log(response)
-        console.log(response.data.base)
-        console.log(response.data.rates)
-        return response
+        return response;
     })
     .catch(error => {
         console.error(error)
-        return error
+        return error;
     })
 }
 
@@ -28,12 +25,14 @@ export function monedaOpenEx(base) {
         console.log(response);
         fx.rates = response.data.rates;
         fx.base = response.data.base;
+        console.log(fx.rates);
+        console.log(fx.base);
         console.log(accounting.toFixed((fx(1000).from("USD").to("GBP")),2));
         return response;
     })
     .catch(error => {
         console.error(error)
-        return error
+        return error;
     })
 }
 
