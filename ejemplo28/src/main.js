5c7a93229ce1681e88a30804d29126911774ba6d
JuanDuran85 <duran85juan@gmail.com>
2020-07-11 12:06:14 -0400

    cambios

--- ejemplo28/src/main.js
+++ ejemplo28/src/main.js
@@ -6,13 +6,7 @@ import Firebase from 'firebase';
 Vue.config.productionTip = false;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0tkMkpfJO8EpdLIXDITp1tvvBIcOV34E",
-  authDomain: "listaejmplo.firebaseapp.com",
-  databaseURL: "https://listaejmplo.firebaseio.com",
-  projectId: "listaejmplo",
-  storageBucket: "listaejmplo.appspot.com",
-  messagingSenderId: "493373771129",
-  appId: "1:493373771129:web:49a4178f6ea73c939b73a8"
+
 };
 
 Firebase.initializeApp(firebaseConfig);
