712f1f51078eacf6a816856db05739f1f7747730
JuanDuran85 <duran85juan@gmail.com>
2020-07-31 17:53:18 -0400

    estilos con bulma

--- ejemplo57/src/App.vue
+++ ejemplo57/src/App.vue
@@ -4,23 +4,26 @@
         <div class="hero-body">
           <h1 class="title">
             <span class="has-text-success">R&M</span>
-            <span class="subtitle">Personajes</span>
+            <span class="subtitle"> Personajes</span>
           </h1>
           <button class="button is-success is-rounded" @click="traerDatos">Consultar</button>
         </div>
       </div>
       <div class="container">
-        <div class="card">
-          <div class="card-header">
-            <img src="" alt="">
-          </div>
-          <div class="card-content">
-            <h3 class="title">Nombre del Personaje</h3>
+        <div class="columns is-desktop is-mobile is-tablet is-multiline is-centered">
+          <div class="column is-12-mobile is-4-desktop is-4-tablet" v-for="(item, index) in datosApi" :key="index">
+            <div class="card">
+              <div class="card-header image figure">
+                <img :src="item.image" :alt="index">
+              </div>
+              <div class="card-content">
+                <h3 class="title is-size-6">{{item.name}}</h3>
+                <button class="button is-success is-rounded is-small">Ver Más</button>
+              </div>
+            </div>
           </div>
         </div>
       </div> 
-      <p>Mostrando datos</p>
-      <p v-if="datosApi">{{datosApi}}</p>
   </div>
 </template>
 
@@ -37,7 +40,7 @@ export default {
   methods: {
     async traerDatos(){
       let resultado = await axios.get('https://rickandmortyapi.com/api/character');
-      console.log(resultado);
+      console.log(resultado.data.results);
       this.datosApi = resultado.data.results;
     }
   },
