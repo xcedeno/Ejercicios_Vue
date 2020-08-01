67d98031102e1cf7358bbe497682ec79b22ab02c
JuanDuran85 <duran85juan@gmail.com>
2020-07-31 22:40:06 -0400

    modal con bulma final

--- ejemplo57/src/App.vue
+++ ejemplo57/src/App.vue
@@ -56,25 +56,6 @@
                         <em>Genero:</em> {{datoPersonaje.gender}}.<br> <em>Estado:</em> {{datoPersonaje.status}}. <br><em>Raza:</em> {{datoPersonaje.species}}.<br> <em>Origen:</em> {{datoPersonaje.origin.name}}
                     </p>
                   </div>
-                  <nav class="level is-mobile">
-                    <div class="level-left">
-                      <a class="level-item" aria-label="reply">
-                        <span class="icon is-small">
-                          <i class="fas fa-reply" aria-hidden="true"></i>
-                        </span>
-                      </a>
-                      <a class="level-item" aria-label="retweet">
-                        <span class="icon is-small">
-                          <i class="fas fa-retweet" aria-hidden="true"></i>
-                        </span>
-                      </a>
-                      <a class="level-item" aria-label="like">
-                        <span class="icon is-small">
-                          <i class="fas fa-heart" aria-hidden="true"></i>
-                        </span>
-                      </a>
-                    </div>
-                  </nav>
                 </div>
               </article>
             </div>
