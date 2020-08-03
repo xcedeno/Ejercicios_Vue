ff98d53f6885496314a4b851d82683ab5d14c83e
JuanDuran85 <duran85juan@gmail.com>
2020-08-03 00:05:55 -0400

    preparando para solicitar info a API

--- ejemplo58/components/PartSelected.js
+++ ejemplo58/components/PartSelected.js
@@ -4,7 +4,7 @@ const PartSelected = {
             <div class="prev-area" @mouseover="hoverPrev = true" @mouseleave="hoverPrev = false">
                 <button class="prev-btn" v-show="hoverPrev" @click="selectPrev">&#10094;</button>
             </div>
-            <img :src="parteSeelccionada.src" :alt="parteSeelccionada.id" class="body-img heads-img about-selected">
+            <img :src="parteSeleccionada.src" :alt="parteSeleccionada.id" class="body-img heads-img about-selected">
             <div class="next-area" @mouseover="hoverNext = true" @mouseleave="hoverNext = false">
                 <button class="next-btn" v-show="hoverNext" @click="selectNext">&#10095;</button>
             </div>
@@ -35,17 +35,25 @@ const PartSelected = {
         selectNext(){
             this.selectedInddex = this.selectedInddex < this.maxIndex ? this.selectedInddex+1 : this.minIndex; 
         },
+        emitSelected(){
+            this.$emit('partSelected',this.parteSeleccionada);
+        }
     },
     computed: {
-        parteSeelccionada(){
+        parteSeleccionada(){
             return this.parts[this.selectedInddex];
         }
     },
     watch: {
         aleatorio(valor){
             if (valor == true){
-                this.selectedInddex = Math.floor(Math.random()*8)
+                this.selectedInddex = Math.floor(Math.random()*7);
+                this.$emit('cambiarSelectedIndex',true);
+                this.emitSelected();
             }
         }
+    },
+    created() {
+        this.emitSelected();
     },
 }
--- ejemplo58/components/the-main.js
+++ ejemplo58/components/the-main.js
@@ -4,8 +4,8 @@ const TheMain = {
     <h1 class="main-title"> Valar Morghulis.. </h1>
     <h2 class="main-subtitle">  Who's next? </h2>
     <div class="row">
-      <!-- <div class="text-center text-md-left" id="quote-area">
-        <div class="spinner-container">
+      <div v-show="isAboutOpen" class="text-center text-md-left" id="quote-area" :class="quotesClass">
+        <div class="spinner-container" v-if="">
           <div class="spinner-grow red-spinner" role="status">
             <span class="sr-only">Loading...</span>
           </div>
@@ -13,17 +13,17 @@ const TheMain = {
         <ul class="quote-list">
           <li></li>
         </ul>
-      </div> -->
+      </div>
       <div class="col-12">
         <div class="ch-body d-flex flex-column">
           <div class="body-row head-row d-flex justify-content-center">
-            <PartSelected :parts="fileParts.heads" :aleatorio="randomHead"></PartSelected>
+            <PartSelected :parts="fileParts.heads" :aleatorio="randomHead" @cambiarSelectedIndex="valor => randomHead = !valor" @partSelected="partSelected"></PartSelected>
           </div>
           <div class="body-row middle-row d-flex justify-content-center">
-            <PartSelected :parts="fileParts.middles" :aleatorio="randomBody"></PartSelected>
+            <PartSelected :parts="fileParts.middles" :aleatorio="randomBody" @cambiarSelectedIndex="valor => randomBody = !valor" @partSelected="partSelected"></PartSelected>
           </div>
           <div class="body-row foot-row d-flex justify-content-center">
-            <PartSelected :parts="fileParts.foots" :aleatorio="randomFoot"></PartSelected>
+            <PartSelected :parts="fileParts.foots" :aleatorio="randomFoot" @cambiarSelectedIndex="valor => randomFoot = !valor" @partSelected="partSelected"></PartSelected>
           </div>
         </div>
         <div class="btn-area d-flex justify-content-center">
@@ -34,7 +34,7 @@ const TheMain = {
           </div>
         </div>
       </div>
-<!--           <div id="about-area">
+      <div v-show="isAboutOpen" id="about-area">
         <div class="spinner-container">
           <div class="spinner-grow red-spinner" role="status">
             <span class="sr-only">Loading...</span>
@@ -49,7 +49,7 @@ const TheMain = {
               <li></li>
           </ul>
         </div>
-      </div> -->
+      </div>
     </div>
   </div>
     `,
@@ -64,7 +64,20 @@ const TheMain = {
               head: {},
               middle: {},
               foot: {}
-            }
+            },
+            isAboutOpen: false,
+            quotesClass: 'col-12 col-md-4 quotes',
+            aboutClass: 'col-12 col-md-4 about',
+            figureClass: 'col-12',
+            selectedChar: {
+              id: 0,
+              name: "",
+              gender: '',
+              born: '',
+              title: [],
+              quote: []
+            },
+            apiCall: false
         }
     },
     methods: {
@@ -72,6 +85,18 @@ const TheMain = {
         this.randomHead = true;
         this.randomBody = true;
         this.randomFoot = true;
+      },
+      partSelected(valor){
+        if (valor.type == 'heads'){
+          this.selectedParts.head = valor;
+          this.selectedCharId = valor.id;
+        };
+        if (valor.type == 'middles') {
+          this.selectedParts.middle = valor;
+        }
+        if (valor.type == 'foots') {
+          this.selectedParts.foot = valor;
+        }
       }
     },
     components: { 
