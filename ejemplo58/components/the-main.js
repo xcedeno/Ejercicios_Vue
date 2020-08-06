787f780aedbb4ef0f44c7ada6149c9fe62909f1b
JuanDuran85 <duran85juan@gmail.com>
2020-08-06 18:55:27 -0400

    mostrando informacion de la APi

--- ejemplo58/components/the-main.js
+++ ejemplo58/components/the-main.js
@@ -5,13 +5,13 @@ const TheMain = {
     <h2 class="main-subtitle">  Who's next? </h2>
     <div class="row">
       <div v-show="isAboutOpen" class="text-center text-md-left" id="quote-area" :class="quotesClass">
-        <div class="spinner-container" v-if="">
+        <div class="spinner-container" v-if="apiCall">
           <div class="spinner-grow red-spinner" role="status">
             <span class="sr-only">Loading...</span>
           </div>
         </div>
         <ul class="quote-list">
-          <li></li>
+          <li v-for="(item,index) in selectedChar.quote" :key="index">{{item}}</li>
         </ul>
       </div>
       <div class="col-12">
@@ -35,18 +35,18 @@ const TheMain = {
         </div>
       </div>
       <div v-show="isAboutOpen" id="about-area" :class="aboutClass">
-        <div class="spinner-container">
+        <div class="spinner-container" v-if="apiCall">
           <div class="spinner-grow red-spinner" role="status">
             <span class="sr-only">Loading...</span>
           </div>
         </div>
-        <div class="text-center text-md-left">
-          <p class="about-p"> <b> Name: </b></p>
-          <p class="about-p"> <b> Born: </b></p>
-          <p class="about-p"> <b> Culture: </b></p>
+        <div class="text-center text-md-left" v-else>
+          <p class="about-p"> <b> Name: </b>{{selectedChar.name}}</p>
+          <p class="about-p"> <b> Born: </b>{{selectedChar.born}}</p>
+          <p class="about-p"> <b> Gender: </b>{{selectedChar.gender}}</p>
           <p class="about-p"> <b> Titles: </b></p>
           <ul class="about-titles">
-              <li></li>
+              <li v-for="(item,index) in selectedChar.titles" :key="index">{{item}}</li>
           </ul>
         </div>
       </div>
@@ -74,7 +74,7 @@ const TheMain = {
               name: "",
               gender: '',
               born: '',
-              title: [],
+              titles: [],
               quote: []
             },
             apiCall: false,
@@ -82,16 +82,53 @@ const TheMain = {
         }
     },
     methods: {
-      async aboutCharacter(){
+      closeAbout(){
+        this.apiCall = false;
+        this.isAboutOpen = false;
+        this.quotesClass = 'col-12 col-md-4 quotes';
+        this.aboutClass = 'col-12 col-md-4 about';
+        this.figureClass = 'col-12';
+      },
+      aboutCharacter(){
         if (this.isAboutSelected) {
           this.apiCall = true;
           this.isAboutOpen = true;
-          
+          this.aboutClass = "col-12 order-1 col-md-4 order-md-3 about area-active";
+          this.quotesClass = "col-12 order-3 col-md-4 order-md-1 about area-active";
+          this.figureClass = "col-12 order-2 col-md-4 order-md-2";
+        }  
+
+        if (this.isAboutOpen && this.selectedParts.head.id !== undefined) {
+          let charApiId = this.selectedParts.head.apiId;
+          if (charApiId !== null){
+            let apiUrl = `https://anapioficeandfire.com/api/characters/${charApiId}`;
+            this.llamandoApi(apiUrl);
+          }
         } else {
-          
+          this.selectedChar.id = this.selectedCharId;
+          this.selectedChar.name = 'No se encuentra el nombre';
+          this.selectedChar.gender = '???';
+          this.selectedChar.born = '???';
+          this.selectedChar.titles = this.selectedParts.head;
+          this.selectedChar.quote = ['No se sabe nada'];
+          this.apiCall = false;
         }
-        let respuesta = await axios.get('https://anapioficeandfire.com/api/characters/');
-        console.log(respuesta.data);
+      },
+      async llamandoApi(apiUrl) {
+        try {
+          let respuesta = await axios.get(apiUrl);
+          console.log(respuesta.data);
+          this.selectedChar.id = this.selectedCharId;
+          this.selectedChar.name = respuesta.data.name;
+          this.selectedChar.gender = respuesta.data.gender;
+          this.selectedChar.born = respuesta.data.born;
+          this.selectedChar.titles = respuesta.data.titles;
+          this.selectedChar.quote = this.selectedParts.head.quotes;
+          this.apiCall = false;
+        } catch (error) {
+          console.error(error);
+          this.apiCall = false;
+        }
       },
       randomAll(){
         this.randomHead = true;
@@ -99,6 +136,7 @@ const TheMain = {
         this.randomFoot = true;
       },
       partSelected(valor){
+        console.log(valor)
         if (valor.type == 'heads'){
           this.selectedParts.head = valor;
           this.selectedCharId = valor.id;
