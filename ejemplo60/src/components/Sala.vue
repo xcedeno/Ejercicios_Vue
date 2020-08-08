433cfae0e1279697c266c05209d50c04a2888c43
JuanDuran85 <duran85juan@gmail.com>
2020-08-07 23:17:45 -0400

    trabajando con el componente principal

--- ejemplo60/src/components/Sala.vue
+++ ejemplo60/src/components/Sala.vue
@@ -1,15 +1,54 @@
 <template>
-  <div>
-      <h2>Sala Componente</h2>
+  <div class="container text-center">
+    <div class="pantalla mt-5">
+      <h2 class="text-white">Pantalla</h2>
+    </div>
+    <div class="asientos mt-5">
+      <div class="row">
+        <div class="col" v-for="(asiento,index) in asientos" :key="index" v-text="asiento.id"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Sala'
+    name: 'Sala',
+    data() {
+      return {
+        asientos: [
+          {
+            id: 'A1',
+            disponible: true
+          },
+          {
+            id: 'A2',
+            disponible: true
+          },
+          {
+            id: 'A3',
+            disponible: true
+          },
+          {
+            id: 'A4',
+            disponible: true
+          },
+          {
+            id: 'A5',
+            disponible: true
+          },
+          {
+            id: 'A6',
+            disponible: true
+          },
+        ]
+      }
+    },
 }
 </script>
 
 <style>
-
+.pantalla {
+  background-color: #5d87be;
+}
 </style>
