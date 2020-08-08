ba062100a568124b925b4528f76bd11c45d863e7
JuanDuran85 <duran85juan@gmail.com>
2020-08-08 18:19:53 -0400

    modificando la base de datos firebase

--- ejemplo60/src/components/Sala.vue
+++ ejemplo60/src/components/Sala.vue
@@ -5,19 +5,24 @@
     </div>
     <div class="asientos mt-5">
       <div class="row">
-        <div class="col text-white font-weight-bold m-3 asiento" v-for="(asiento,index) in asientos" :key="index" v-text="asiento.id" :class="asiento.disponible ? 'disponible' : 'ocupado'" @click="selecionAsiento" :id="asiento.id"></div>
+        <div class="col text-white font-weight-bold m-3" v-for="(asiento,index) in asientos" :key="index" v-text="asiento.id" :class="[asiento.disponible ? 'disponible' : 'ocupado', {'asiento': !asiento.adquirido}]" @click="selecionAsiento" :id="asiento.id"></div>
       </div>
+    </div>
+    <div>
+      <b-button variant="success" @click="guardar" class="mt-5">Guardar</b-button>
     </div>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase';
+
 export default {
     name: 'Sala',
     data() {
       return {
         asientos: [
-          {
+/*           {
             id: 'A1',
             disponible: true
           },
@@ -40,15 +45,49 @@ export default {
           {
             id: 'A6',
             disponible: true
-          },
+          }, */
         ]
       }
     },
     methods: {
+      asientosSeleccionados(){
+        return this.asientos.filter(ocupado => !ocupado.disponible && !ocupado.adquirido)
+      },
+      validarSientos(){
+
+      },
+      guardar(){
+        this.actualizarElementos();
+      },
+      cargarDatos(data){
+        this.asientos = data;
+      },
       selecionAsiento(event){
         let asientoSeleccionado = this.asientos.find(item => item.id === event.target.id);
+        if (asientoSeleccionado.adquirido) {
+          console.log("Asiento ocupado "+asientoSeleccionado.id);
+          return
+        }
         asientoSeleccionado.disponible = !asientoSeleccionado.disponible;
-      }
+      },
+      actualizarElementos(){
+        /* firebase.database().ref('/salas/sala1').set(this.asientos); */
+        firebase.database().ref('salas').child('sala1').set(this.asientos, (error)=>{
+          if (error) {
+            console.error(error);
+          } else {
+            console.log("Operación correcta");
+          }
+        }).then( () => {
+          console.log("Actualización finalizada");
+        });
+      },
+    },
+    created() {
+      //this.actualizarElementos();
+      firebase.database().ref('salas').child('sala1').once('value', snapshot => {
+        this.cargarDatos(snapshot.val())
+      })
     },
 }
 </script>
@@ -57,14 +96,12 @@ export default {
   .pantalla {
     background-color: #5d87be;
   }
-
   .asiento{
     cursor: pointer;
   }
   .disponible{
     background-color: rgb(33, 84, 131);
   }
-
   .ocupado{
     background-color: crimson;
   }
--- ejemplo60/src/main.js
+++ ejemplo60/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import firebase from 'firebase';
+import {firebaseConfig} from './config/firebaseConfig';
+
+firebase.initializeApp(firebaseConfig);
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
