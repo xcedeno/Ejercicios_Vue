d4a722bc0f1a256a69c3dd0e26e03c122da68f72
JuanDuran85 <duran85juan@gmail.com>
2020-08-10 00:00:43 -0400

    trabajando con variables de entorno

--- ejemplo60/src/main.js
+++ ejemplo60/src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import firebase from 'firebase';
 import {firebaseConfig} from './config/firebaseConfig';
 import Notifications from 'vue-notification';
 
 firebase.initializeApp(firebaseConfig);
+
+console.log(process.env.VUE_APP_EJEMPLO_KEY);
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
