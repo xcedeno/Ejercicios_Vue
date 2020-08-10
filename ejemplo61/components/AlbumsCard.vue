5b16162b8578c9cc4a2af21cb5778df3fd7279ed
JuanDuran85 <duran85juan@gmail.com>
2020-08-10 13:59:02 -0400

    ejemplo usando NuxtJS, VueJS y Bulma

---
+++ ejemplo61/components/AlbumsCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <ul>
+          <li>{{album.id}}-{{album.title}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AlbumsCard',
+    props: {
+        album: { 
+            type: Object,
+            required: true 
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ ejemplo61/pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div class="tile is-parent">
+        <article class="tile is-child notification is-info">
+          <p class="title">Ejemplo con NuxtJS y VueJS</p>
+          <p class="subtitle">Información de Albums</p>
+            <albums-card v-for="(item,index) in albums" :key="index" :album="item"></albums-card>
+        </article>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import env from '../config/env';
+import albumsCard from '../components/AlbumsCard.vue';
+
+export default {
+    name: 'Index',
+    data() {
+        return {
+            albums: [],
+        }
+    },
+    created() {
+        axios.get(`${env.apiUrl}/albums`).then(response=>{
+            console.log(response);
+            this.albums = response.data;
+        })
+    },
+    components: {
+        albumsCard
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
