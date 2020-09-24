41cfa6d0a9643141f4e0d823596239ab9096da75
JuanDuran85 <duran85juan@gmail.com>
2020-09-24 18:47:56 -0300

    agregando ordenes

--- ejemplo62/src/components/MonitorOrdenes.vue
+++ ejemplo62/src/components/MonitorOrdenes.vue
@@ -4,10 +4,45 @@
       <form>
         <div class="form-row">
           <div class="col">
-            <input type="text" class="form-control" placeholder="First name">
+            <div class="form-group">
+              <label for="exampleFormControlSelect1">Oficina</label>
+              <select class="form-control" id="exampleFormControlSelect1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </div>
           </div>
           <div class="col">
-            <input type="text" class="form-control" placeholder="Last name">
+            <div class="form-group">
+              <label for="exampleFormControlSelect1">Estado</label>
+              <select class="form-control" id="exampleFormControlSelect1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </div>
+          </div>
+          <div class="col">
+            <div class="form-group">
+              <label for="example-datepicker">Fecha de Entrega</label>
+              <b-form-datepicker id="example-datepicker" v-model="value" class="mb-2"></b-form-datepicker>
+            </div>
+          </div>
+          <div class="col">
+            <div class="form-group">
+              <label for="example-datepicker">a</label>
+              <b-form-datepicker id="example-datepicker" v-model="value" class="mb-2"></b-form-datepicker>
+            </div>
+          </div>
+           <div class="col">
+            <div class="form-group mt-4">
+              <b-button variant="primary" class="mt-4" block>Buscar</b-button>
+            </div>
           </div>
         </div>
       </form>
