c15bbf29bfea30df21eec75fe624bccba26fde14
JuanDuran85 <duran85juan@gmail.com>
2021-02-06 19:11:57 -0300

    configurando el proyecto

--- ejemplo68/src/main.js
+++ ejemplo68/src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
-import store from './store';
+import router from './router/router';
+import store from './store/store';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faCheck);
+library.add(faTimes);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false
 
--- ejemplo68/src/store/store.js
+++ ejemplo68/src/store/store.js
@@ -1,15 +1,19 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+  
+  },
+  getters: {
+  
   },
   mutations: {
+  
   },
   actions: {
+  
   },
-  modules: {
-  }
 })
